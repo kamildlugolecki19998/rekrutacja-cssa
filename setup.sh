4846190e2ebf79f3874ee40987d2570c7fba1873
Kamil Dlugolecki <kamildlugolecki1998@gmail.com>
2025-01-08 10:29:17 +0100

    Change setup.sh

--- setup.sh
+++ setup.sh
@@ -14,11 +14,15 @@ sleep 10  # Dajemy trochę czasu na uruchomienie kontenera
 
 # Wykonanie komendy composer install w kontenerze
 echo "Running composer install inside the container..."
-docker exec -it cs-php -c "composer install"
+# docker exec -it cs-php -c "composer install"
+
+#docker exec -it cs-php sh -c "composer install --prefer-dist --no-scripts --no-interaction"
+docker exec -it cs-php sh -c "composer install --prefer-dist --no-scripts --no-interaction"
+
 
 # Dodatkowe kroki, jeśli są wymagane (np. migracja bazy danych)
-# echo "Running database migrations..."
-# docker exec -it symfony_app bash -c "php bin/console doctrine:migrations:migrate --no-interaction"
+ echo "Running database migrations..."
+ docker exec -it cs-php sh -c "php bin/console doctrine:migrations:migrate --no-interaction"
 
 # Informacja o zakończeniu
 echo "Setup complete!"
